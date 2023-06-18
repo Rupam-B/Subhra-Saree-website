e13eec48b716a8f36e1fe9f54369ae31b6897a88
Rupam Banerjee <rupam.banerje@gmail.com>
2023-06-18 17:09:48 +0530

    Payment link added

---
+++ script.js
@@ -0,0 +1,34 @@
+function purchaseGateway() {
+    window.location.assign("purchase.html")
+}
+
+// Function set for View Images
+function showImage1() {
+    window.location.assign("images/disp-1.jpg")
+}
+function showImage2() {
+    window.location.assign("images/disp-2.jpg")
+}
+function showImage3() {
+    window.location.assign("images/disp-3.jpg")
+}
+function showImage4() {
+    window.location.assign("images/disp-4.jpg")
+}
+function showImage5() {
+    window.location.assign("images/disp-5.jpg")
+}
+function showImage6() {
+    window.location.assign("images/disp-6.jpg")
+}
+function showImage7() {
+    window.location.assign("images/disp-7.jpg")
+}
+function showImage8() {
+    window.location.assign("images/disp-8.jpg")
+}
+function showImage9() {
+    window.location.assign("images/disp-9.jpg")
+}
+// ///////////////-------------///////////////
+
